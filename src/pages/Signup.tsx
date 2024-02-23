faeb90063bcbc47eba1c6e504bb3a2bc77aff641
kleneway <kleneway@gmail.com>
2024-02-22 16:48:16 -0800

    updates

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,42 +1,43 @@
-import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/router';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const SignupPage = () => {
   const router = useRouter();
 
   const handleSignUp = async () => {
-    const res = await signIn('github');
+    const res = await signIn("github");
     if (res?.url) {
-      router.push('/setup'); // Navigate to the setup page
+      await router.push("/setup"); // Navigate to the setup page
     } else {
       // Display error to the user
-      alert('An error occurred during sign up. Please try again.');
+      alert("An error occurred during sign up. Please try again.");
     }
   };
 
   return (
-    <div className="flex flex-col md:flex-row w-full h-screen">
-      <div className="md:w-1/2 flex flex-col justify-center items-center bg-white px-10 md:px-20 py-20">
-        <h1 className="text-4xl font-semibold text-not-black-black mb-4">
+    <div className="flex h-screen w-full flex-col md:flex-row">
+      <div className="flex flex-col items-center justify-center bg-white px-10 py-20 md:w-1/2 md:px-20 ">
+        <h1 className="text-not-black-black mx-auto mb-4 max-w-lg  text-4xl font-semibold">
           Turn your designs into deployable code
         </h1>
-        <p className="text-lg text-not-black-black">
-          The AI-powered coding assistant that turns your designs into deployable code
+        <p className="text-not-black-black mx-auto max-w-lg text-lg">
+          The AI-powered coding assistant that turns your designs into
+          deployable code
         </p>
       </div>
-      <div className="md:w-1/2 flex flex-col justify-center items-center bg-beige px-10 md:px-20 py-20">
+      <div className="flex flex-col items-center justify-center bg-beige px-10 py-20 md:w-1/2 md:px-20">
         <div className="w-full max-w-xl">
-          <h2 className="text-3xl text-not-black-black mb-2">
+          <h2 className="text-not-black-black mb-2 text-3xl">
             Access the Beta
           </h2>
-          <p className="text-lg text-dark-beige mb-10">
+          <p className="mb-10 text-lg text-zinc-500">
             Join the community of engineers guiding the development of JACoB
           </p>
           <button
             onClick={handleSignUp}
-            className="w-full py-2.5 bg-violet-beauregarde text-white rounded-lg font-medium flex justify-center items-center"
+            className="bg-violet-beauregarde flex w-full items-center justify-center rounded-lg py-2.5 font-medium text-white"
           >
             <FontAwesomeIcon icon={faGithub} className="mr-2" />
             Sign Up With GitHub
